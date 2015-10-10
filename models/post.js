173233fe1603f85f9c1445dc64f23379b1438d50
Nicolas Roos <nicolas.k.roos@gmail.com>
2015-10-10 11:49:00 +0200

    renmaned title property to name

--- models/post.js
+++ models/post.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var PostSchema = new Schema({
-  title: String,
+  name: String,
   text: String,
   author: String,
   created: Number,
