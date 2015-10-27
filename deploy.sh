831cb6c068dfc4379903606792137fc02e7ba736
Nicolas Roos <nicolas.k.roos@gmail.com>
2015-10-27 20:51:31 +0100

    please

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@
 set -ev
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
 	echo "Master Branch"
-	cd dist
-	scp -i deploy -r . deploy@vsg-xebaci.cyoncloud.com:/var/www/html/diekulissezug.ch
+	scp -i deploy -r dist/* deploy@vsg-xebaci.cyoncloud.com:/var/www/html/diekulissezug.ch
 	ssh -i deploy deploy@vsg-xebaci.cyoncloud.com 'cd /var/www/html/diekulissezug.ch/ | npm install --production'
 fi
