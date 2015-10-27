b8d98a3b0810f3427b11688bc5c052ef6827a4dc
Nicolas Roos <nicolas.k.roos@gmail.com>
2015-10-27 22:35:55 +0100

    proper build for dev

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,9 @@ if [ "${TRAVIS_BRANCH}" = "master" ]; then
 	scp -i deploy -r dist/* deploy@vsg-xebaci.cyoncloud.com:/var/www/html/diekulissezug.ch
 	ssh -i deploy deploy@vsg-xebaci.cyoncloud.com 'cd /var/www/html/diekulissezug.ch && npm install --production'
 fi
+
+if [ "${TRAVIS_BRANCH}" = "development" ]; then
+	echo "Dev Branch"
+	scp -i deploy -r dist/* deploy@vsg-xebaci.cyoncloud.com:/var/www/html/dkz.gifstr.io
+	ssh -i deploy deploy@vsg-xebaci.cyoncloud.com 'cd /var/www/html/dkz.gifstr.io && npm install --production'
+fi
