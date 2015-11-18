cb2018bc64887af19099115297cbf59e6451be6a
Nicolas Roos <nicolas.k.roos@gmail.com>
2015-11-18 20:54:44 +0100

    changed deploy path for master

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 set -ev
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
 	echo "Master Branch"
-	scp -i deploy -r dist/* deploy@vsg-xebaci.cyoncloud.com:/var/www/html/diekulissezug.ch
-	ssh -i deploy deploy@vsg-xebaci.cyoncloud.com 'cd /var/www/html/diekulissezug.ch && npm install --production'
+	scp -i deploy -r dist/* deploy@vsg-xebaci.cyoncloud.com:/var/www/html/www.diekulissezug.ch
+	ssh -i deploy deploy@vsg-xebaci.cyoncloud.com 'cd /var/www/html/www.diekulissezug.ch && npm install --production'
 fi
 
 if [ "${TRAVIS_BRANCH}" = "development" ]; then
